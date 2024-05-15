61b4d95eafbf7546aed921cd61dd75b5493a1615
Hermann Máté Béla <mate3829@gmail.com>
2024-05-15 12:18:46 +0200

    homeframe gameframe
    bakfitty

--- projlab_pupakok/src/Views/HomeFrame.java
+++ projlab_pupakok/src/Views/HomeFrame.java
@@ -1,6 +1,10 @@
 package Views;
+import java.awt.*;
+import javax.swing.*;
 
 public class HomeFrame extends JFrame {
+    private final int height = 500;
+    private final int width = 315;
     private JLabel nameLabel = new JLabel();
     private JTextField hallgatoCount = new JTextField();
     private JButton startButton = new JButton("Start");
@@ -9,15 +13,27 @@ public class HomeFrame extends JFrame {
 
 
     public HomeFrame() {
+        setSize(height, width);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
         initComponents();
     }
     private void initComponents() {
-        setLayout(new BorderLayout());
-        startButton.addActionListener(e -> {
-            // TODO
-        });
-        exitButton.addActionListener(e -> {
-            // TODO
-        });
+        this.setLayout(new BorderLayout());
+        panel.setLayout(new GridBagLayout());
+
+        startButton.addActionListener(e -> {new GameFrame(); this.setVisible(false);});
+        panel.add(startButton);
+
+        exitButton.addActionListener(e -> System.exit(0));
+        panel.add(exitButton);
+
+        this.add(panel);
+
+        this.setVisible(true);
+
+    
     }
+
 }
